b3bbc490935f2f99265d4137cfb8f01376acbc3c
Calvin Giles <calvin.giles@gmail.com>
2014-08-29 13:52:42 +0100

    /api now accepts and url params and json post data and returns it along with the result. #9

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,12 +9,15 @@ def hello():
 
 
 
-@app.route('/api')
+@app.route('/api', methods=['GET', 'POST'])
 def form():
+    print(request.args)
     dummy_results = {"data":[{"date": "2014-07-01", "amount": 290},
                              {"date": "2014-08-01", "amount": 210},
                              {"date": "2014-09-01", "amount": 210},
                              {"date": "2014-10-01", "amount": 140},
                              {"date": "2014-11-01", "amount": 80},
-                             ]}
+                             ],
+                     "args":request.args,
+                     "json":request.get_json(force=True)}
     return jsonify(**dummy_results)
