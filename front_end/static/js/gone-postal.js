cb97a4d1be397b012397ded8ce496776d2527162
Calvin Giles <calvin.giles@gmail.com>
2014-08-30 18:34:31 +0100

    Moved dummy request json to gone-postal.js. Now the server-side interface is complete, the front and back-ends can be developed indipendently again. Relevant to #8 and #9. Closes #11

--- front_end/static/js/gone-postal.js
+++ front_end/static/js/gone-postal.js
@@ -8,10 +8,17 @@
     
 //I think this is the code needed to read in data from a JSON file, it doesn't work locally because of browser security. ReQuires a small change to the html     
     app.controller('findResults', function($scope,$http){
-        results = this
-                   $http.post('monthly_sample.json', {})
+        result = this
+        dummy = {
+                  "savings_targets": [{"name": "NY trip","amount": 120.00,"date": "2014-09-12"},
+                                      {"name": "New shoes","amount": 1300.00,"date": "2014-11-03"}
+                                     ],
+                  "existing_savings": 134.26,
+                  "session_id": "ae98-08c2-bd39-a167"
+                }
+                   $http.post('monthly_sample.json', dummy)
                         .then(function(res){
-                            results.items = res.data.result_data
+                            result.items = res.data.result_data
                     });
     });
         
--- main.py
+++ main.py
@@ -19,19 +19,10 @@ def route_form():
     return app.send_static_file('form.html')
 
 
-dummy_request_json = {
-  "savings_targets": [{"name": "NY trip","amount": 120.00,"date": "2014-09-12"},
-                      {"name": "New shoes","amount": 1500.00,"date": "2014-11-03"}
-                     ],
-  "existing_savings": 134.26,
-  "session_id": "ae98-08c2-bd39-a167"
-}
-
-
 @app.route('/monthly_sample.json', methods=['GET', 'POST'])
 def form():
     request_json = request.get_json()
-    result_json = calculate_result(dummy_request_json)
+    result_json = calculate_result(request_json)
     result_dict = store_results(request.args, request_json, result_json, collection_name)
     return jsonify(**result_dict)
 
